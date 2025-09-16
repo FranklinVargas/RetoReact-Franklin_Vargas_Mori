0ffe142243a03143c5c89dbd7b8d8fbd8819c85d
Franklin Vargas Mori <81585870+FranklinVargas@users.noreply.github.com>
2025-09-16 03:18:22 -0500

    1.3

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,25 +1,15 @@
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
-dotenv.config();
 
-export const sequelize = new Sequelize(
+const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT,
     dialect: "mysql",
-    logging: false
+    logging: false,
   }
 );
 
-export async function connectDB() {
-  try {
-    await sequelize.authenticate();
-    console.log("✅ DB conectada");
-  } catch (err) {
-    console.error("❌ Error conectando DB:", err.message);
-    process.exit(1);
-  }
-}
+export default sequelize;
